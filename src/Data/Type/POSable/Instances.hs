342639a5437e4fc94ff604231334ebaf42fe9c56
Rick van Hoef <rickvhoef@gmail.com>
2022-03-11 10:11:44 +0100

    generate instances of POSable for tuples up to length 16

--- src/Data/Type/POSable/Instances.hs
+++ src/Data/Type/POSable/Instances.hs
@@ -1,11 +1,12 @@
 {-# LANGUAGE DeriveAnyClass       #-}
 {-# LANGUAGE StandaloneDeriving   #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE TemplateHaskell      #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 {-# OPTIONS_GHC -fplugin GHC.TypeLits.KnownNat.Solver #-}
 -- This is needed to derive POSable for tuples of size more then 4
-{-# OPTIONS_GHC -fconstraint-solver-iterations=8 #-}
+{-# OPTIONS_GHC -fconstraint-solver-iterations=16 #-}
 
 -- | This module contains instances of `POSable` for all Haskell prelude data types
 --   as well as fixed size integers from Data.Int (`Int8`, `Int16`, `Int32` and `Int64`)
@@ -14,6 +15,7 @@ module Data.Type.POSable.Instances (POSable) where
 import           Data.Int                         (Int16, Int32, Int64, Int8)
 import           Data.Type.POSable.POSable
 import           Data.Type.POSable.Representation
+import Language.Haskell.TH
 
 -----------------------------------------------------------------------
 -- Instances for common nonrecursive Haskell datatypes
@@ -23,10 +25,18 @@ deriving instance (POSable l, POSable r) => POSable (Either l r)
 deriving instance POSable Ordering
 
 deriving instance POSable ()
-deriving instance (POSable a, POSable b) => POSable (a,b)
-deriving instance (POSable a, POSable b, POSable c) => POSable (a,b,c)
-deriving instance (POSable a, POSable b, POSable c, POSable d) => POSable (a,b,c,d)
-deriving instance (POSable a, POSable b, POSable c, POSable d, POSable e) => POSable (a,b,c,d,e)
-deriving instance (POSable a, POSable b, POSable c, POSable d, POSable e, POSable f) => POSable (a,b,c,d,e,f)
-deriving instance (POSable a, POSable b, POSable c, POSable d, POSable e, POSable f, POSable g) => POSable (a,b,c,d,e,f,g)
-deriving instance (POSable a, POSable b, POSable c, POSable d, POSable e, POSable f, POSable g, POSable h) => POSable (a,b,c,d,e,f,g,h)
+
+-- Instances for tuples of length 2 - 16
+runQ $ do
+  let
+    mkTuple :: Int -> Q Dec
+    mkTuple n =
+      let
+          xs  = [ mkName ('x' : show i) | i <- [0 .. n-1] ]
+          ts  = map varT xs
+          res = foldl (\ts' t -> [t| $ts' $t |]) (tupleT (length ts)) ts
+          ctx = mapM (appT [t| POSable |]) ts
+      in
+      instanceD ctx [t| POSable $res |] []
+  mapM mkTuple [2..16]
+
